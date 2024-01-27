10347d02253bf6531a494d65f43c1b5480544472
Nicolas Hidalgo <nicolas.hidalgo.1989@gmail.com>
2024-01-27 01:53:31 -0300

    feat: lat lng zoom |  marker

--- src/app/maps/pages/markers-page/markers-page.component.ts
+++ src/app/maps/pages/markers-page/markers-page.component.ts
@@ -1,9 +1,48 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { Map, LngLat, Marker } from 'mapbox-gl';
 
 @Component({
   templateUrl: './markers-page.component.html',
   styleUrl: './markers-page.component.css'
 })
 export class MarkersPageComponent {
+
+  @ViewChild('map') divMap?: ElementRef;
+
+  public zoom: number = 13;
+  public map?: Map;
+  public lngLat: LngLat = new LngLat(11.96, 57.71);
+  public lng?: number;
+  public lat?: number;
+
+  constructor(){
+    const { lng, lat } = this.lngLat;
+    this.lng = lng;
+    this.lat = lat;
+  }
+
+  ngAfterViewInit(): void {
+    if(!this.divMap) throw new Error('Method not implemented.');
+
+    this.map = new Map({
+      accessToken: 'pk.eyJ1IjoibmhpZGFsZ28iLCJhIjoiY2xydXN4cmZiMGxmcDJqcDF1dmZvMXJ2dSJ9.B4NFxW9NxiC3qwcluSw4UQ',
+      container: this.divMap?.nativeElement,
+      style: 'mapbox://styles/mapbox/streets-v12',
+      center: this.lngLat,
+      zoom: this.zoom,
+    });
+
+    // const markerHtml = document.createElement('div');
+    // markerHtml.innerHTML = 'Nic';
+
+
+    // const marker = new Marker({
+    //   color: 'purple',
+    //   element: markerHtml
+    // })
+    //   .setLngLat( this.lngLat )
+    //   .addTo( this.map )
+
+  }
 
 }
--- src/app/maps/pages/zoom-page/zoom-page.component.ts
+++ src/app/maps/pages/zoom-page/zoom-page.component.ts
@@ -1,26 +1,36 @@
-import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
-import { Map } from 'mapbox-gl';
+import { AfterViewInit, Component, ElementRef, OnDestroy, ViewChild } from '@angular/core';
+import { Map, LngLat } from 'mapbox-gl';
 
 @Component({
   templateUrl: './zoom-page.component.html',
   styleUrl: './zoom-page.component.css'
 })
-export class ZoomPageComponent implements AfterViewInit{
+export class ZoomPageComponent implements AfterViewInit, OnDestroy{
+
 
   @ViewChild('map') divMap?: ElementRef;
 
   public zoom: number = 10;
   public map?: Map;
+  public lngLat: LngLat = new LngLat(11.96, 57.71);
+  public lng?: number;
+  public lat?: number;
+
+  constructor(){
+    const { lng, lat } = this.lngLat;
+    this.lng = lng;
+    this.lat = lat;
+  }
 
   ngAfterViewInit(): void {
     if(!this.divMap) throw new Error('Method not implemented.');
 
     this.map = new Map({
       accessToken: 'pk.eyJ1IjoibmhpZGFsZ28iLCJhIjoiY2xydXN4cmZiMGxmcDJqcDF1dmZvMXJ2dSJ9.B4NFxW9NxiC3qwcluSw4UQ',
-      container: this.divMap?.nativeElement, // container ID
-      style: 'mapbox://styles/mapbox/streets-v12', // style URL
-      center: [-74.5, 40], // starting position [lng, lat]
-      zoom: this.zoom, // starting zoom
+      container: this.divMap?.nativeElement,
+      style: 'mapbox://styles/mapbox/streets-v12',
+      center: this.lngLat,
+      zoom: this.zoom,
     });
 
     this.mapListeners();
@@ -36,6 +46,12 @@ export class ZoomPageComponent implements AfterViewInit{
       if(this.map!.getZoom() < 18) return;
       this.map!.zoomTo(18);
     })
+    this.map.on('move', () => {
+      this.lngLat = this.map!.getCenter();
+      const { lng, lat } = this.lngLat;
+      this.lng = lng;
+      this.lat = lat;
+    })
   }
 
   zoomIn(){
@@ -49,6 +65,10 @@ export class ZoomPageComponent implements AfterViewInit{
   changeZoom(value: string){
    this.zoom = Number(value);
    this.map?.zoomTo(this.zoom);
+  }
+
+  ngOnDestroy(): void {
+    this.map?.remove();
   }
 
 }
