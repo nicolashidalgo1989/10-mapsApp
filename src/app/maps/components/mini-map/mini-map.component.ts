615296a12c14c5da349f8850e6c611b8ba9fe73d
Nicolas Hidalgo <nicolas.hidalgo.1989@gmail.com>
2024-01-29 03:45:31 -0300

    feat: mini map card

--- src/app/maps/components/mini-map/mini-map.component.ts
+++ src/app/maps/components/mini-map/mini-map.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, Input, ViewChild } from '@angular/core';
+import { Map, Marker } from 'mapbox-gl';
 
 @Component({
   selector: 'maps-mini-map',
   templateUrl: './mini-map.component.html',
   styleUrl: './mini-map.component.css'
 })
-export class MiniMapComponent {
+export class MiniMapComponent implements AfterViewInit{
+
+  @Input() lngLat?: [number, number];
+
+  @ViewChild('map') divMap?: ElementRef;
+
+  ngAfterViewInit(): void {
+
+    if(!this.divMap) throw "Map not found";
+    if(!this.lngLat) throw "lngLat not found";
+
+    const map = new Map({
+      accessToken: 'pk.eyJ1IjoibmhpZGFsZ28iLCJhIjoiY2xydXN4cmZiMGxmcDJqcDF1dmZvMXJ2dSJ9.B4NFxW9NxiC3qwcluSw4UQ',
+      container: this.divMap?.nativeElement,
+      style: 'mapbox://styles/mapbox/streets-v12',
+      center: this.lngLat,
+      zoom: 15,
+      interactive: false
+    });
+
+    new Marker()
+      .setLngLat( this.lngLat)
+      .addTo( map )
+
+  }
 
 }
